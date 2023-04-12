01d304b7b7a67555d125e781b1f5c7499845dad6
jarli <jarli@DENODEN>
2023-04-12 18:14:05 +0200

    Добавьте файлы проекта.

---
+++ les_1/les_1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    system("start https://github.com/sunmeat");
+    
+    
+    
+    
+    //system("type C:\\Users\\jarli\\Рабочий стол\\Комп курсы\\с++\\les_1\\hi.txt");
+    
+    
+    /*system("color E9");
+    setlocale(0, "UKR");
+    cout << "Привіт світ!\n";
+    cout << "\tМене звати \\Сашко\\.\n";
+    cout << "\t\tЯ працюю в комп. \"IT Step\"!\n";
+    */
+}
+
+
+/*
+int main() {
+    cout << "\n\t\"\To be\n\n";
+    cout << "\t\tor not\n\n";
+    cout << "\t\t\tto be...\"\n\n";
+    cout << "\t\t\t\t \/ Shakespeare /\n\n";
+
+}
+*/
